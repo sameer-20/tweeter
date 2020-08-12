3c060efbfa27db8bff8b2583cc3d6bad3f1469c2
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-08-12 17:01:26 +0000

    Modified eventhandler

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,9 +3,8 @@
 const $maxLength = 140;
 
 $(document).ready(function() {
-    //$('#tweet-text').on('keypress', clickHandler);
 
-    $('#tweet-text').keypress(function() {
+    $('#tweet-text').keyup(function() {
       const $textLength = $(this).val().length;
       const $counter = $(this).siblings('.tweet-submit').children('.counter');
       const $currentCount = $maxLength - $textLength;
@@ -13,23 +12,6 @@ $(document).ready(function() {
 
       $currentCount < 0 ? $counter.css('color', 'red') : $counter.css('color', '#444444');
 	  	
-
     });
 });
 
-
-
-/* const clickHandler = function (event) {
-  let length = $(this).val().length;
-  length = maxLength-length;
-  if (length >= 0) {
-    $('.counter').text(length);
-  } else {
-    $('.counter').text(length).css({"color" : "red"});
-  }
-  
-  //console.log(this.parent);
-  //$('this').children();
-  //$(this).parent().children().
-   
-}; */
